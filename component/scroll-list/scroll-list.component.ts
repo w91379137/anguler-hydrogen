2dac7f30621966356ccaa4f98372fbd97b7590e8
w91379137 <w913791379137@gmail.com>
2022-10-31 22:25:28 +0800

    [feat] 調整滾動層級

--- component/scroll-list/scroll-list.component.ts
+++ component/scroll-list/scroll-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { ScrollItemViewModel, ScrollListViewModel } from './scroll-list.viewmodel';
 
 @Component({
@@ -6,10 +6,19 @@ import { ScrollItemViewModel, ScrollListViewModel } from './scroll-list.viewmode
   templateUrl: './scroll-list.component.html',
   styleUrls: ['./scroll-list.component.scss']
 })
-export class ScrollListComponent implements OnInit {
+export class ScrollListComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('card_container')
+  container: ElementRef
+
+  @ViewChildren('card_div')
+  cardList: QueryList<ElementRef>;
 
   @Input()
   public viewModel: ScrollListViewModel;
+
+  @Output()
+  scrolled = new EventEmitter<ScrollItemViewModel[]>();
 
   @Output()
   cardClicked = new EventEmitter<ScrollItemViewModel>();
@@ -19,9 +28,45 @@ export class ScrollListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.onScrolled()
+    }, 1);
   }
 
   // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+  onScrolled() {
+    // 這邊可以查哪個只元件在畫面上
+    // 然後去回報
+    // console.log()
+    let parentPos = this.container.nativeElement.getBoundingClientRect()
+
+    let show: ScrollItemViewModel[] = []
+    // console.log('位置', scrollTop)
+    for (const card of this.cardList) {
+
+      // https://stackoverflow.com/questions/26423335/elements-coordinates-relative-to-its-parent
+
+      let childPos = card.nativeElement.getBoundingClientRect()
+      let top = childPos.top - parentPos.top + 1 // 讓第 0 個能出現
+      let bottom = childPos.bottom - parentPos.bottom - 1 // 讓最後一個 能出現
+      // console.log('計算', top, bottom)
+      if (top > 0 && bottom < 0) {
+
+        let id = parseInt(card.nativeElement.id)
+        // console.log('取得', id, top, bottom)
+        let cardVM = this.viewModel.itemList.find(ele => ele.id === id)
+        if (cardVM) {
+          show.push(cardVM)
+        }
+      }
+    }
+    this.scrolled.emit(show)
+  }
+
   onCardClicked(item: ScrollItemViewModel) {
     this.cardClicked.emit(item)
   }
