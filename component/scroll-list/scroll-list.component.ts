f777cf57a385601fc49b0206ae615bc8c8df7d5d
w91379137 <w913791379137@gmail.com>
2023-01-17 20:43:43 +0800

    [feat] 簡化回傳資料, 優化顯示判斷

--- component/scroll-list/scroll-list.component.ts
+++ component/scroll-list/scroll-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, QueryList, TemplateRef, ViewChild, ViewChildren } from '@angular/core';
 import { interval, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { ScrollItemViewModel, ScrollListViewModel } from './scroll-list.viewmodel';
@@ -12,6 +12,13 @@ export class ScrollListComponent implements OnInit, AfterViewInit, OnDestroy {
 
   private ngUnsubscribe = new Subject<void>();
 
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+
+  @Input()
+  customCard: TemplateRef<any>;
+
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+
   @ViewChild('card_container')
   container: ElementRef
 
@@ -21,8 +28,10 @@ export class ScrollListComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input()
   public viewModel: ScrollListViewModel;
 
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+
   @Output()
-  scrolled = new EventEmitter<ScrollItemViewModel[]>();
+  scrolled = new EventEmitter<number[]>();
 
   @Output()
   cardClicked = new EventEmitter<ScrollItemViewModel>();
@@ -69,26 +78,20 @@ export class ScrollListComponent implements OnInit, AfterViewInit, OnDestroy {
     // 這邊可以查哪個只元件在畫面上
     // 然後去回報
     // console.log()
-    let parentPos = this.container.nativeElement.getBoundingClientRect()
+    let parentRect: DOMRect = this.container.nativeElement.getBoundingClientRect()
 
-    let show: ScrollItemViewModel[] = []
+    let show: number[] = []
     // console.log('位置', scrollTop)
     for (const card of this.cardList) {
 
       // https://stackoverflow.com/questions/26423335/elements-coordinates-relative-to-its-parent
 
-      let childPos = card.nativeElement.getBoundingClientRect()
-      let top = childPos.top - parentPos.top + 1 // 讓第 0 個能出現
-      let bottom = childPos.bottom - parentPos.bottom - 1 // 讓最後一個 能出現
-      // console.log('計算', top, bottom)
-      if (top > 0 && bottom < 0) {
+      let childRect: DOMRect = card.nativeElement.getBoundingClientRect()
+      if (intersectRect(parentRect, childRect)) {
 
         let id = parseInt(card.nativeElement.id)
         // console.log('取得', id, top, bottom)
-        let cardVM = this.viewModel.itemList.find(ele => ele.id === id)
-        if (cardVM) {
-          show.push(cardVM)
-        }
+        show.push(id)
       }
     }
     this.scrolled.emit(show)
@@ -97,4 +100,11 @@ export class ScrollListComponent implements OnInit, AfterViewInit, OnDestroy {
   onCardClicked(item: ScrollItemViewModel) {
     this.cardClicked.emit(item)
   }
+}
+
+function intersectRect(rect1: DOMRect, rect2: DOMRect) {
+  return !(rect2.left > rect1.right ||
+           rect2.right < rect1.left ||
+           rect2.top > rect1.bottom ||
+           rect2.bottom < rect1.top);
 }
