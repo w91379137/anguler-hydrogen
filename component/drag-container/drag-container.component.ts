698e5cc766aba82e4f03a05b02f6fa7e189a7f2f
w91379137 <w913791379137@gmail.com>
2023-03-12 02:13:30 +0800

    [feat] 調整 浮動按鈕 增加 邊界設定

--- component/drag-container/drag-container.component.ts
+++ component/drag-container/drag-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'h2-drag-container',
@@ -9,6 +9,9 @@ export class DragContainerComponent implements OnInit {
 
   // https://stackoverflow.com/questions/56925391/differentiate-cdkdrag-and-click-in-angular
   public contentClickable = true
+
+  @Input()
+  boundary = { top: '0', bottom: '0', left: '0', right: '0' }
 
   @Output()
   DragStart = new EventEmitter<void>();
