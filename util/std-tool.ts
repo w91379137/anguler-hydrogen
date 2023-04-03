3c07535ac42cff4efb0bd2e806b9c9a5798a11ed
w91379137 <w913791379137@gmail.com>
2023-04-04 02:00:14 +0800

    [feat] 調整時間可以帶入

--- util/std-tool.ts
+++ util/std-tool.ts
@@ -36,26 +36,30 @@ export function clearDict(
 }
 
 // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
-export function TimezoneNextMidnight(timeZoneOffset: number): Date {
+
+/** 取得某時區在某個時間點之後的下一個午夜 */
+export function TimezoneNextMidnight(
+  timeZoneOffset: number,
+  targetTime: Date = new Date()
+): Date {
 
   // https://zh.wikipedia.org/wiki/%E6%97%B6%E5%8C%BA%E5%88%97%E8%A1%A8
   // 時區是 -12 ~ +14
 
   // 當下時間 保留年月日
-  const result = new Date();
+  const result = new Date(targetTime.getTime());
 
   // 直接改成該時區的小時
   const hour = Math.floor(timeZoneOffset);
   const min = Math.floor((timeZoneOffset - hour) / 0.25) * 15;
-  // console.log(hour, min);
+  console.log(hour, min);
 
   // 讓這個時區的 午夜 12點 變成交界
   result.setUTCHours(-hour, -min, 0, 0);
 
-  // 直接與當下比較
-  const now = new Date();
+  // 找到超過 targetTime 的下一個午夜
   let limit = 0;
-  while (now.getTime() > result.getTime() && limit < 10) {
+  while (targetTime.getTime() > result.getTime() && limit < 10) {
     result.setDate(result.getDate() + 1);
     limit ++;
   }
