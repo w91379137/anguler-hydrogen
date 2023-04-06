34914842b64dc03e752d30daf13579bd011e0a86
w91379137 <w913791379137@gmail.com>
2023-04-07 04:56:49 +0800

    [fix] 修正使用箭頭函數 導致綁定錯誤

--- decorator/ignore-before-finish/ignore-before-finish-decorator.ts
+++ decorator/ignore-before-finish/ignore-before-finish-decorator.ts
@@ -1,59 +1,80 @@
 
 import { noop, Observable } from "rxjs"
 import { timeout } from "rxjs/operators"
-import { delay } from "../../util/std-tool"
+import { delay, randomInt } from "../../util/std-tool"
 
 const BusyRecordDict = { }
 
 export function IgnoreBeforeFinish(
   {
-    uuid = `${Math.random()}`,
+    uuid = `${randomInt(9999)}`,
     timeoutms = 1000,
   }) {
 
   let result = function (
-    target: Object,
-    key: string | symbol, // 名稱
+    target: Object, // 目標物件(不要用這個 anguler 還會繼續 di 還有其他 Decorator)
+    key: string | symbol, // 函數名稱
     descriptor: PropertyDescriptor
   ) {
 
-    const childFunction = descriptor.value
+    const oldFunction: Function = descriptor.value
+    // 這邊禁止使用箭頭函數 因為會把目前的 this 綁定到這邊
+    const newFunction: Function = function(...args: any[]) {
 
-    descriptor.value = (...args: any[]) => {
+      let name = 'no name'
+      if (typeof key === 'string') {
+        name = key
+      }
+      if (typeof key === 'symbol') {
+        name = Symbol.keyFor(key)
+      }
+
       if (BusyRecordDict[uuid]) {
-        let name = ''
-        if (typeof key === 'string') {
-          name = key
-        }
-        if (typeof key === 'symbol') {
-          name = Symbol.keyFor(key)
-        }
         console.log(`Busy_Group.uuid:${uuid}_function:${name}`)
         return null
       }
 
-      const enable = () => { BusyRecordDict[uuid] = false }
-
-      let result = childFunction.apply(this, args)
-      if (typeof result.subscribe === 'function') {
+      // 開始執行
+      const disable = () => {
         BusyRecordDict[uuid] = true
+        // console.log(`disable.uuid:${uuid}_function:${name}`)
+      }
+
+      const enable = (error: any = undefined) => {
+        BusyRecordDict[uuid] = false
+        // console.log(`enable.uuid:${uuid}_function:${name}`)
+        if (error) {
+          console.warn(error)
+        }
+      }
+
+      // console.log('操作物件 target:', this.constructor.name)
+      let result = oldFunction.apply(this, args)
+
+      if (typeof result.subscribe === 'function') {
+        disable()
         let _ = (<Observable<any>>result)
         .pipe(timeout(timeoutms))
         .subscribe({
           next: noop,
-          complete: enable,
-          error: enable
+          complete: () => { enable() },
+          error: (error) => { enable(error) }
         })
       } else if (typeof result.then === 'function') {
-        BusyRecordDict[uuid] = true
+        disable()
         Promise.race([
           <Promise<any>>result,
           delay(timeoutms),
         ])
-        .then(enable).catch(enable)
+        .then(() => { enable() })
+        .catch((error) => { enable(error)})
+      }
+      else {
+        console.log('IgnoreBeforeFinish: 未知的回傳型態', result)
       }
       return result
     }
+    descriptor.value = newFunction
     return descriptor
   }
   return result
