d05d8cfcdf56803fa7b3bfd36508704423a3eee7
w91379137 <w913791379137@gmail.com>
2023-05-05 18:29:20 +0800

    [feat] 新增 k m 相關數字顯示

--- pipe/PointFmt/point-fmt.pipe.ts
+++ pipe/PointFmt/point-fmt.pipe.ts
@@ -3,14 +3,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 const DefaultBasePointDivisor = 10000
 export let BasePointDivisor = DefaultBasePointDivisor
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+// https://segmentfault.com/a/1190000043627884
+
+const formatter = new Intl.NumberFormat('en-US', {
+  notation: 'compact',
+} as any )
 @Pipe({
   name: 'pointFmt'
 })
 export class PointFmtPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: number, isCompact: number = 0, ...args: unknown[]): unknown {
     if (typeof value !== 'number') {
       return ''
+    }
+
+    if (isCompact) {
+      return formatter.format(value / BasePointDivisor)
     }
     return (value / DefaultBasePointDivisor).toLocaleString();
   }
