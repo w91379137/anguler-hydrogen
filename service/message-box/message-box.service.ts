50023d86f1a0f338589a67955781c8ffbd248606
w91379137 <w913791379137@gmail.com>
2024-03-15 20:41:49 +0800

    [feat] 嚴格要求 alert 需要點中間

--- service/message-box/message-box.service.ts
+++ service/message-box/message-box.service.ts
@@ -2,28 +2,29 @@ import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { AlertComponent } from '../../component/alert/alert.component';
-import { AlertViewModel, AlertTitle, AlertButtonMode } from '../../component/alert/alert.viewmodel';
+import {
+  AlertViewModel,
+  AlertTitle,
+  AlertButtonMode,
+} from '../../component/alert/alert.viewmodel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessageBoxService {
-
-  constructor(
-    protected dialog: MatDialog
-  ) { }
+  constructor(protected dialog: MatDialog) {}
 
   // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
   // action
 
   showAlert(
     data: AlertViewModel,
-    disableClose: boolean = false,
+    disableClose: boolean = true
   ): Observable<AlertViewModel | undefined> {
     const dialogRef = this.dialog.open(AlertComponent, {
       data,
       panelClass: '',
-      width: '350px'
+      width: '350px',
     });
 
     dialogRef.disableClose = disableClose;
@@ -33,31 +34,40 @@ export class MessageBoxService {
   showAlertSuccessError(
     isSuccess: boolean,
     successMessage: string | string[],
-    warningMessage: string | string[]): Observable<AlertViewModel | undefined> {
-      return isSuccess ? this.showAlertSuccess(successMessage) : this.showAlertError(warningMessage)
+    warningMessage: string | string[]
+  ): Observable<AlertViewModel | undefined> {
+    return isSuccess
+      ? this.showAlertSuccess(successMessage)
+      : this.showAlertError(warningMessage);
   }
 
-  showAlertSuccess(message: string | string[]): Observable<AlertViewModel | undefined> {
-    let vm = new AlertViewModel()
-    vm.title = AlertTitle.Success
-    vm.messageList = Array.isArray(message) ? message : [message]
-    vm.mode = AlertButtonMode.onlyyes
+  showAlertSuccess(
+    message: string | string[]
+  ): Observable<AlertViewModel | undefined> {
+    let vm = new AlertViewModel();
+    vm.title = AlertTitle.Success;
+    vm.messageList = Array.isArray(message) ? message : [message];
+    vm.mode = AlertButtonMode.onlyyes;
     return this.showAlert(vm);
   }
 
-  showAlertWarning(message: string | string[]): Observable<AlertViewModel | undefined> {
-    let vm = new AlertViewModel()
-    vm.title = AlertTitle.Warning
-    vm.messageList = Array.isArray(message) ? message : [message]
-    vm.mode = AlertButtonMode.yesno
+  showAlertWarning(
+    message: string | string[]
+  ): Observable<AlertViewModel | undefined> {
+    let vm = new AlertViewModel();
+    vm.title = AlertTitle.Warning;
+    vm.messageList = Array.isArray(message) ? message : [message];
+    vm.mode = AlertButtonMode.yesno;
     return this.showAlert(vm);
   }
 
-  showAlertError(message: string | string[]): Observable<AlertViewModel | undefined> {
-    let vm = new AlertViewModel()
-    vm.title = AlertTitle.Error
-    vm.messageList = Array.isArray(message) ? message : [message]
-    vm.mode = AlertButtonMode.onlyyes
+  showAlertError(
+    message: string | string[]
+  ): Observable<AlertViewModel | undefined> {
+    let vm = new AlertViewModel();
+    vm.title = AlertTitle.Error;
+    vm.messageList = Array.isArray(message) ? message : [message];
+    vm.mode = AlertButtonMode.onlyyes;
     return this.showAlert(vm);
   }
 }
